a077fa9d826e2fec76ecb3d6bf05ef2b57e60db3
wooody92 <pkower4@naver.com>
2020-09-24 15:00:28 +0900

    feat : WebClient 주식 데이터 요청 및 응답 테스트
    - webClient - blocking 설정 후 동기적으로 데이터 요청 및 응답 테스트

--- src/main/java/dev/banksalad/stock/service/Sample.java
+++ src/main/java/dev/banksalad/stock/service/Sample.java
@@ -1,16 +1,25 @@
 package dev.banksalad.stock.service;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
 @Getter
+@Setter
 @ToString
 public class Sample {
 
     private String date;
-    private Double open;
+
+    @JsonProperty(value = "price")
     private Double close;
-    private Double high;
-    private Double low;
+
     private Long volume;
+
+    public Sample(String date, Double close, Long volume) {
+        this.date = date;
+        this.close = close;
+        this.volume = volume;
+    }
 }
--- src/main/java/dev/banksalad/stock/service/SampleService.java
+++ src/main/java/dev/banksalad/stock/service/SampleService.java
@@ -1,19 +1,37 @@
 package dev.banksalad.stock.service;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Service
 public class SampleService {
 
     @Autowired
-    RestTemplateBuilder restTemplateBuilder;
+    WebClient.Builder builder;
 
-    public Sample[] getData() {
-        RestTemplate restTemplate = restTemplateBuilder.rootUri("https://sandbox.iexapis.com").build();
-        Sample[] data = restTemplate.getForObject("/stable/stock/AAPL/chart/1m?token=Tpk_14ad95bb91954d929f4d657bbcb51d58", Sample[].class);
-        return data;
+    public List<Sample> getData() {
+        WebClient webClient = builder.baseUrl("https://sandbox.iexapis.com").build();
+
+        List<Sample> sampleList = webClient.get()
+            .uri(uriBuilder -> uriBuilder
+                .path("/stable")
+                .path("/stock")
+                .path("/APPL/chart/1m")
+                .queryParam("token", "Tpk_14ad95bb91954d929f4d657bbcb51d58")
+                .build())
+            .retrieve()
+            .bodyToFlux(Sample.class)
+            .collectList()
+            .block();
+
+        return sampleList;
     }
+
 }
