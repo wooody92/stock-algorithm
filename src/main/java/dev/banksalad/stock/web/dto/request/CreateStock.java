9cda61d26bdaa55f8b3d0266259cfc45c36be4f1
wooody92 <pkower4@naver.com>
2020-09-25 16:37:28 +0900

    feat : CreateStock DTO 생성
    - Stock Entity 객체 생성을 위한 DTO 생성했습니다.
    - build 값은 수정 예정입니다.

---
+++ src/main/java/dev/banksalad/stock/web/dto/request/CreateStock.java
@@ -0,0 +1,36 @@
+package dev.banksalad.stock.web.dto.request;
+
+import dev.banksalad.stock.domain.stock.Stock;
+import java.time.LocalDate;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CreateStock {
+
+    private String name;
+    private Double profit;
+    private LocalDate date;
+    private LocalDate buyDate;
+    private LocalDate sellDate;
+
+    @Builder
+    public CreateStock(String name, Double profit, LocalDate date, LocalDate buyDate,
+        LocalDate sellDate) {
+        this.name = name;
+        this.profit = profit;
+        this.date = date;
+        this.buyDate = buyDate;
+        this.sellDate = sellDate;
+    }
+
+    public static Stock toEntity() {
+        return Stock.builder()
+            .name("nameTest")
+            .profit(1234d)
+            .build();
+    }
+}
