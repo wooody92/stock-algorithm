c1ef6a88a1248b1f0951b5220d098b66b01eb444
wooody92 <pkower4@naver.com>
2020-09-25 16:01:27 +0900

    test : StockRunner 생성
    - 간단한 테스트를 위한 StockRunner 생성 했습니다.
    - 테스트를 위해 IexCloudEnum.PATH_MONTH를 1달로 변경했습니다.

--- src/main/java/dev/banksalad/stock/iextrading/IexCloudEnum.java
+++ src/main/java/dev/banksalad/stock/iextrading/IexCloudEnum.java
@@ -7,7 +7,7 @@ public enum IexCloudEnum {
     BASE_URL("https://sandbox.iexapis.com/stable"),
     PATH_STOCK("/stock/"),
     PATH_CHART("/chart"),
-    PATH_MONTH("/6m"),
+    PATH_MONTH("/1m"),
     TOKEN("token"),
     PUBLIC_KEY("Tpk_14ad95bb91954d929f4d657bbcb51d58");
 
