c86e9632cc5e1ed3a5018d7e7aa6aa65a0f1c344
wooody92 <pkower4@naver.com>
2020-09-25 20:35:58 +0900

    feat : StockInformationDto 생성
    - 주식 정보 응답을 위한 response DTO 생성

---
+++ src/main/java/dev/banksalad/stock/web/dto/response/StockInformationDto.java
@@ -0,0 +1,45 @@
+package dev.banksalad.stock.web.dto.response;
+
+import dev.banksalad.stock.iextrading.IexCloud;
+import java.time.LocalDate;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class StockInformationDto {
+
+    private String symbol;
+    private LocalDate date;
+    private Double open;
+    private Double close;
+    private Double high;
+    private Double low;
+    private Long volume;
+
+    @Builder
+    public StockInformationDto(String symbol, LocalDate date, Double open, Double close,
+        Double high, Double low, Long volume) {
+        this.symbol = symbol;
+        this.date = date;
+        this.open = open;
+        this.close = close;
+        this.high = high;
+        this.low = low;
+        this.volume = volume;
+    }
+
+    public static StockInformationDto of(String symbol, IexCloud iexCloud) {
+        return StockInformationDto.builder()
+            .symbol(symbol)
+            .date(iexCloud.getDate())
+            .open(iexCloud.getOpen())
+            .close(iexCloud.getClose())
+            .high(iexCloud.getHigh())
+            .low(iexCloud.getLow())
+            .volume(iexCloud.getVolume())
+            .build();
+    }
+}
