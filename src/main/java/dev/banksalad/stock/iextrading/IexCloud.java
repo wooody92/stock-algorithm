3aaca12a0354ec02bebc4a79899977104a5d4792
wooody92 <pkower4@naver.com>
2020-09-25 20:34:08 +0900

    feat : IexCloud response 필드값 추가
    - 데이터 가공을 위한 필드값 추가

--- src/main/java/dev/banksalad/stock/iextrading/IexCloud.java
+++ src/main/java/dev/banksalad/stock/iextrading/IexCloud.java
@@ -1,28 +1,29 @@
 package dev.banksalad.stock.iextrading;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import java.time.LocalDate;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.ToString;
 
 @Getter
+@ToString
 public class IexCloud {
 
-    private String date;
-
-    @JsonProperty(value = "price")
+    private LocalDate date;
+    private Double open;
     private Double close;
-
+    private Double high;
+    private Double low;
     private Long volume;
 
-    public IexCloud(String date, Double close, Long volume) {
+    @Builder
+    public IexCloud(LocalDate date, Double open, Double close, Double high, Double low,
+        Long volume) {
         this.date = date;
+        this.open = open;
         this.close = close;
+        this.high = high;
+        this.low = low;
         this.volume = volume;
-    }
-
-    @Override
-    public String toString() {
-        return "{\"date\":" + "\"" +date + "\""
-            + ",\"volume\":" + volume
-            + ",\"price\":" + close + "}";
     }
 }
