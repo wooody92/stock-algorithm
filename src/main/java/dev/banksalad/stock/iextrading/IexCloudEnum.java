a03793f5c1c38f59cf3fb5f935caae2c04553c47
wooody92 <pkower4@naver.com>
2020-09-25 15:06:08 +0900

    refactor : IexCloudService.getData 상수값 Enum 처리
    - IexCloudEnum 생성하여 상수값을 Enum으로 관리합니다.

---
+++ src/main/java/dev/banksalad/stock/iextrading/IexCloudEnum.java
@@ -0,0 +1,19 @@
+package dev.banksalad.stock.iextrading;
+
+import lombok.Getter;
+
+@Getter
+public enum IexCloudEnum {
+    BASE_URL("https://sandbox.iexapis.com/stable"),
+    PATH_STOCK("/stock/"),
+    PATH_CHART("/chart"),
+    PATH_MONTH("/6m"),
+    TOKEN("token"),
+    PUBLIC_KEY("Tpk_14ad95bb91954d929f4d657bbcb51d58");
+
+    private String value;
+
+    IexCloudEnum(String value) {
+        this.value = value;
+    }
+}
--- src/main/java/dev/banksalad/stock/service/IexCloudService.java
+++ src/main/java/dev/banksalad/stock/service/IexCloudService.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import static dev.banksalad.stock.iextrading.IexCloudEnum.*;
+
 @Service
 @RequiredArgsConstructor
 public class IexCloudService {
@@ -13,20 +15,19 @@ public class IexCloudService {
     private final WebClient.Builder builder;
 
     public List<IexCloud> getData(String symbol) {
-        WebClient webClient = builder.baseUrl("https://sandbox.iexapis.com/stable").build();
-
+        WebClient webClient = builder.baseUrl(BASE_URL.getValue()).build();
         List<IexCloud> iexCloudList = webClient.get()
             .uri(uriBuilder -> uriBuilder
-                .path("/stock/")
+                .path(PATH_STOCK.getValue())
                 .path(symbol)
-                .path("/chart/6m")
-                .queryParam("token", "Tpk_14ad95bb91954d929f4d657bbcb51d58")
+                .path(PATH_CHART.getValue())
+                .path(PATH_MONTH.getValue())
+                .queryParam(TOKEN.getValue(), PUBLIC_KEY.getValue())
                 .build())
             .retrieve()
             .bodyToFlux(IexCloud.class)
             .collectList()
             .block();
-
         return iexCloudList;
     }
 }
