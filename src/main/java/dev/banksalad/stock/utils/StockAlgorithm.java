de7110b168e507360e9bbe76db8f68bfe2534dd7
wooody92 <pkower4@naver.com>
2020-09-26 17:50:20 +0900

    feat : StockAlgorithm.getMaxProfitAndDate 생성
    - 가격 리스트와 날짜 리스트를 받아 최대 수익과 해당 날짜를 구하는 메서드 생성

---
+++ src/main/java/dev/banksalad/stock/utils/StockAlgorithm.java
@@ -0,0 +1,38 @@
+package dev.banksalad.stock.utils;
+
+import dev.banksalad.stock.web.dto.request.CreateProfit;
+import java.time.LocalDate;
+import java.util.List;
+
+public class StockAlgorithm {
+
+    public static CreateProfit getMaxProfitAndDate(List<Double> price, List<LocalDate> dates) {
+        double maxProfit = 0;
+        double minPrice = price.get(0);
+        int purchaseIndex = -1;
+        int saleIndex = -1;
+
+        for (int i = 1; i < price.size(); i++) {
+            Double profit = price.get(i) - minPrice;
+            if (profit > maxProfit) {
+                maxProfit = profit;
+                saleIndex = i;
+            }
+            if (price.get(i) < minPrice) {
+                minPrice = price.get(i);
+            }
+        }
+        for (int i = 0; i < saleIndex; i++) {
+            if (price.get(saleIndex) - price.get(i) == maxProfit) {
+                purchaseIndex = i;
+                break;
+            }
+        }
+        return CreateProfit.builder()
+            .profit(maxProfit)
+            .date(dates.get(dates.size() - 1))
+            .purchaseDate(dates.get(purchaseIndex))
+            .saleDate(dates.get(saleIndex))
+            .build();
+    }
+}
