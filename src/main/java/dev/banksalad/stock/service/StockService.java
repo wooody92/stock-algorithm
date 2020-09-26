4819c028fa15932a8e7f46043466c81486d0e04d
wooody92 <pkower4@naver.com>
2020-09-26 17:57:16 +0900

    feat : StockService.getMaxProfitDate 생성
    - StockInformationDto -> StockAlgorithm -> Profit 엔티티를 만드는 메서드 생성

--- src/main/java/dev/banksalad/stock/service/StockService.java
+++ src/main/java/dev/banksalad/stock/service/StockService.java
@@ -1,8 +1,13 @@
 package dev.banksalad.stock.service;
 
+import dev.banksalad.stock.domain.profit.Profit;
 import dev.banksalad.stock.iextrading.IexCloud;
 import dev.banksalad.stock.repository.StockRepository;
+import dev.banksalad.stock.utils.StockAlgorithm;
+import dev.banksalad.stock.web.dto.request.CreateProfit;
 import dev.banksalad.stock.web.dto.response.StockInformationDto;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -18,5 +23,32 @@ public class StockService {
     public List<StockInformationDto> getStockInformation(String symbol) {
         List<IexCloud> iexClouds = iexCloudService.requestData(symbol);
         return iexCloudService.getStockData(symbol, iexClouds);
+    }
+
+    public void getMaxProfitDate(String symbol) {
+        List<StockInformationDto> stockInformation = getStockInformation(symbol);
+        List<Double> price = parsePrice(stockInformation);
+        List<LocalDate> dates = parseDate(stockInformation);
+
+        CreateProfit createProfit = StockAlgorithm.getMaxProfitAndDate(price, dates);
+        Profit profit = CreateProfit.toEntity(createProfit);
+
+        System.out.println(profit);
+    }
+
+    private List<Double> parsePrice(List<StockInformationDto> stockInformation) {
+        List<Double> price = new ArrayList<>();
+        for (StockInformationDto data : stockInformation) {
+            price.add(data.getClose());
+        }
+        return price;
+    }
+
+    private List<LocalDate> parseDate(List<StockInformationDto> stockInformation) {
+        List<LocalDate> dates = new ArrayList<>();
+        for (StockInformationDto data : stockInformation) {
+            dates.add(data.getDate());
+        }
+        return dates;
     }
 }
