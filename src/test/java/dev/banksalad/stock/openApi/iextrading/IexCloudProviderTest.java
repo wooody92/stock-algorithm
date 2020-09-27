804652eec5ea1552f6952024e7e93a51efbd9e44
wooody92 <pkower4@naver.com>
2020-09-27 20:27:49 +0900

    test : IexCloudProviderTest.getLatestRecordDateTest 테스트 구현
    - 외부 API 데이터의 최신 날짜가 한국 시간 오늘 기준으로 5일 이내인지 확인하는 테스트

--- src/test/java/dev/banksalad/stock/openApi/iextrading/IexCloudProviderTest.java
+++ src/test/java/dev/banksalad/stock/openApi/iextrading/IexCloudProviderTest.java
@@ -2,6 +2,7 @@ package dev.banksalad.stock.openApi.iextrading;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.time.LocalDate;
 import java.util.List;
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
@@ -31,5 +32,17 @@ public class IexCloudProviderTest {
         assertThat(iexClouds.get(0).getClose()).isNotNull();
         assertThat(iexClouds.get(iexClouds.size() - 1).getDate()).isNotNull();
         assertThat(iexClouds.get(iexClouds.size() - 1).getClose()).isNotNull();
+    }
+
+    @Test
+    @DisplayName("외부 API 데이터의 최신 날짜가 한국 시간 오늘 기준으로 5일 이내인지 확인하는 테스트")
+    public void getLatestRecordDateTest() {
+        //given, when
+        LocalDate latestRecordDate = iexCloudProvider.getLatestRecordDate(STOCK_SYMBOL);
+
+        //then
+        assertThat(latestRecordDate).isNotNull();
+        assertThat(latestRecordDate)
+            .isBetween(LocalDate.now().minusDays(5), LocalDate.now());
     }
 }
