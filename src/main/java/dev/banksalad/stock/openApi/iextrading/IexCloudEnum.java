fa45492667a84310cbe1bbe1727bd656caf0c6e0
wooody92 <pkower4@naver.com>
2020-09-27 23:03:59 +0900

    config : IexCloud API request base url 변경
    - mock -> 실 api url 로 변경

--- src/main/java/dev/banksalad/stock/openApi/iextrading/IexCloudEnum.java
+++ src/main/java/dev/banksalad/stock/openApi/iextrading/IexCloudEnum.java
@@ -4,10 +4,10 @@ import lombok.Getter;
 
 @Getter
 public enum IexCloudEnum {
-    BASE_URL("https://sandbox.iexapis.com/stable"),
+    BASE_URL("https://cloud.iexapis.com/stable"),
     PATH_STOCK("/stock/"),
     PATH_CHART("/chart"),
-    PATH_MONTH("/1m"),
+    PATH_MONTH("/6m"),
     PATH_DAYS("/5d"),
     SORT("sort"),
     DESC("desc"),
