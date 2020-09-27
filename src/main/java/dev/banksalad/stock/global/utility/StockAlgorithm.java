2d7c06865b2da4166bfb40d5711e99c12718106e
wooody92 <pkower4@naver.com>
2020-09-27 18:57:57 +0900

    refactor : StockAlgorithm.getMaxProfitAndDate 최대 수익이 0인 경우 고려
    - 가격 변동이 없거나, 계속 떨어지기만 한다면 maxProfit = 0.0 입니다. 해당 경우 purchaseDate, salDate는 dates의 처음 날짜로 반환 합니다.

--- src/main/java/dev/banksalad/stock/global/utility/StockAlgorithm.java
+++ src/main/java/dev/banksalad/stock/global/utility/StockAlgorithm.java
@@ -7,11 +7,15 @@ import java.util.List;
 
 public class StockAlgorithm {
 
+    /**
+     * 가격 변동이 없거나, 계속 떨어지기만 한다면 maxProfit = 0.0 입니다.
+     * 해당 경우 purchaseDate, salDate는 dates의 처음 날짜로 반환 합니다.
+     */
     public static CreateProfit getMaxProfitAndDate(List<Double> price, List<LocalDate> dates) {
         double maxProfit = 0;
         double minPrice = price.get(0);
-        int purchaseIndex = -1;
-        int saleIndex = -1;
+        int purchaseIndex = 0;
+        int saleIndex = 0;
 
         for (int i = 1; i < price.size(); i++) {
             Double profit = price.get(i) - minPrice;
