a5683b14db7a87c2cfd38a607cdc25e349ca4ddc
wooody92 <pkower4@naver.com>
2020-10-24 17:19:47 +0900

    refactor : 최대 수익 알고리즘 로직 테스트 개선
    - 난수를 이용하여 하드코딩 지양
    - 시간복잡도가 O(N^2)인 알고리즘을 이용하여 비즈니스 로직과 비교하여 검증

--- src/test/java/dev/banksalad/stock/web/dto/StockInformationTest.java
+++ src/test/java/dev/banksalad/stock/web/dto/StockInformationTest.java
@@ -5,11 +5,13 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 import dev.banksalad.stock.domain.stock.Profit;
 import dev.banksalad.stock.openApi.iextrading.IexCloud;
 import dev.banksalad.stock.web.dto.response.StockInformationDto;
+import java.text.DecimalFormat;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -93,5 +95,68 @@ public class StockInformationTest {
                 .isEqualTo(LocalDate.parse("2020-01-03")),
             () -> assertThat(maxProfitAndDate[4].getSaleDate()).isEqualTo(LocalDate.parse("2020-01-05"))
         );
+    }
+
+    @Test
+    @RepeatedTest(50)
+    @DisplayName("최대 수익과 최대 수익을 내는 매수, 매도일을 서로 다른 알고리즘 비교를 통해 검증하는 테스트")
+    public void improvedGetMaxProfitAndDateTest() {
+        //given
+        final int DATA_LENGTH = 100;
+        List<StockInformationDto> info = new ArrayList<>();
+        for (int i = 0; i < DATA_LENGTH; i++) {
+            LocalDate date = LocalDate.now().minusDays(DATA_LENGTH).plusDays(i);
+            Double price = getSecondDigit(Math.random() * 100);
+            info.add(StockInformationDto.builder()
+                .date(date)
+                .close(price)
+                .build());
+        }
+        StockInformation stockInformation = StockInformation.of(info);
+
+        //when
+        Profit profit = stockInformation.getMaxProfitAndDate();
+        Profit profitByTestAlgorithm = maxProfitAlgorithm(stockInformation);
+
+        //then
+        assertThat(profit.getProfit()).isEqualTo(profitByTestAlgorithm.getProfit());
+        assertThat(profit.getDate()).isEqualTo(profitByTestAlgorithm.getDate());
+        assertThat(profit.getPurchaseDate()).isEqualTo(profitByTestAlgorithm.getPurchaseDate());
+        assertThat(profit.getSaleDate()).isEqualTo(profitByTestAlgorithm.getSaleDate());
+    }
+
+    /**
+     * 테스트 검증을 위한 비교 알고리즘
+     * 시간 복잡도: O(N^2)
+     */
+    private Profit maxProfitAlgorithm(StockInformation stockInformation) {
+        List<Double> price = stockInformation.getPrice();
+        List<LocalDate> dates = stockInformation.getDate();
+        double maxProfit = 0;
+        int purchaseIndex = 0;
+        int saleIndex = 0;
+
+        for (int i = 0; i < price.size() - 1; i++) {
+            for (int j = i + 1; j < price.size(); j++) {
+                double profit = price.get(j) - price.get(i);
+                if (profit > maxProfit) {
+                    purchaseIndex = i;
+                    saleIndex = j;
+                    maxProfit = profit;
+                }
+            }
+        }
+        return Profit.builder()
+            .date(dates.get(dates.size() - 1))
+            .profit(getSecondDigit(maxProfit))
+            .purchaseDate(dates.get(purchaseIndex))
+            .saleDate(dates.get(saleIndex))
+            .build();
+    }
+
+    private Double getSecondDigit(Double digit) {
+        DecimalFormat decimalFormat = new DecimalFormat("#.##");
+        String format = decimalFormat.format(digit);
+        return Double.parseDouble(format);
     }
 }
